4073c00a7578f869f7918de9dab52de7957ce255
babruix <romanalexey@gmail.com>
2015-02-16 22:30:25 +0100

    Brick fixes

--- js/collections/Brick.class.js
+++ js/collections/Brick.class.js
@@ -1,4 +1,4 @@
-var Brick = function() {
+var Brick = function () {
   var x2 = game.rnd.integerInRange(0, game.width);
   var y2 = game.rnd.integerInRange(0, game.height);
   this.brick = game.add.sprite(x2, y2, 'brick');
@@ -7,16 +7,16 @@ var Brick = function() {
   game.physics.p2.enable(this.brick, debug);
   //this.brick.body.data.gravityScale = 0.05;
   //this.brick.body.static = true;
-  this.brick.scale.setTo(.5,.5);
+  this.brick.scale.setTo(.5, .5);
 
   SpaceGame._bricks.add(this.brick);
 
   this.brick.body.onBeginContact.add(function (body1, shapeA, shapeB) {
-    if (body1 && body1.sprite != null && body1.sprite.key=='spaceship') {
+    if (body1 && body1.sprite != null && body1.sprite.key == 'spaceship') {
       // do not kill pickable items, only tower can pickup.
       // @todo: enemy can pickup?
       this.brick.kill();
-      body1.sprite.countBricks+=10;
+      body1.sprite.countBricks += 1;
       updateScoreText();
     }
   }, this);
